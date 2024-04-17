f014e0e9b924c186132ecf9af3d8015cbe4c304d
s30482 <162422031+s30482@users.noreply.github.com>
2024-04-17 09:19:08 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 public class Main {
         public static void Main(String[] args) {
             ArrayList<employee> employees = new ArrayList<>();
-// Creating and adding developers
+
             Developer dev = new Developer("John", "Doe", "New York", "john@doe.com", "123456789", 2020);
             Technology t=new Technology("Java", 800);
             dev.addTechnology(t);
             employees.add(dev);
-// Creating and adding testers
+
             Tester tester = new Tester("Jane", "Smith", "LosAngeles", "jane@smith.com", "987654321", 2021);
             tester.addTestType("UI/UX");
             employees.add(tester);
-// Creating and adding a manager
+
             Manager manager = new Manager("Bob", "Johnson",
                     "Chicago", "bob@johnson.com", "1122334455", 2019);
             Goal g=new Goal(2010, 10, 15, "Implementing FB login",
                     1000);
             manager.addGoal(g);
             employees.add(manager);
-// Calculating total amount to be paid
+
             int totalAmount = 0;
             for (employee employee : employees) {
                 totalAmount += employee.calculateSalary();
